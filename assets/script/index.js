2c9d29c4bfb7c0a9ac452bff398abe1e18af38c9
Adrian Zhao <achnnal@gmail.com>
2023-03-16 14:19:54 -0500

    css done

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,3 +4,19 @@
   Yuhan Zhao
 */
 
+class Person {
+  #name;
+  #city;
+  #email;
+  constructor(name, city, email) {
+    this.#name = name;
+    this.#city = city;
+    this.#email = email;
+  }
+  set name(name) { this.#name = name; }
+  set city(city) { this.#city = city; }
+  set email(email) { this.#email = email; }
+  get name() { return this.#name; } 
+  get city() { return this.#city; }
+  get email() { return this.#email; }
+}
